13df0a63418b585a850845595641c991d13257a2
Elie Dagher <125106050+eliasdagger@users.noreply.github.com>
2026-08-20 19:11:34 -0400

    implemented functions to ingest csv data, creating indications as functions, unpack the functions to create normalized scoring. done in a hashmap: Dict[str, List[int]]. Current scores are placeholders

--- src/score.py
+++ src/score.py
@@ -1,7 +1,8 @@
 import os
+import csv
 # Represent each 11 indications represented as a vector R7 of unmet needs.
 
-indications = [
+INDICATIONS = [
     "GBM",
     "AZ",
     "IPF",
@@ -15,9 +16,18 @@ indications = [
     "BCC"
 ]
 
-def create_indication(indications: List[str]):
-    vectors = {}
-    with ("/data/raw_scores.csv", "r") as c:
+DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "raw_scores.csv")
 
-        for indication in indications: 
-            vectors[indication] = [w for w in ]
+def create_indications_as_vector():
+    indication_vectors = {}
+    with open(DATA_PATH, "r") as c:
+        reader = csv.reader(c)
+        header = next(reader)
+        
+        for indication in reader: 
+            indication_vectors[indication[0]] = [w for w in indication[1:]]
+            print(f"Created {indication[0]} as a vector ")
+
+    return indication_vectors
+
+

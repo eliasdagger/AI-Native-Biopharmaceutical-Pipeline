bc41915f689e0aef073db740ffa18d9be9145b39
Elie Dagher <125106050+eliasdagger@users.noreply.github.com>
2026-08-21 21:08:25 -0400

    created methods to create vector objects of the indications and unmet needs, prepared for normalization

--- src/normalize.py
+++ src/normalize.py
@@ -1,38 +1,7 @@
 import os
 import csv
-from score import create_indications_as_vector, INDICATIONS
+from score import create_indications_as_vector, create_unmet_score_vectors
 from typing import Dict, List
 
-unmet_needs = [
-    "clinical_severity",
-    "therepeutic_effectiveness_gap",
-    "access_and_affordability_gap",
-    "mechanistic_coverage_gap",
-    "competitive_density",
-    "repurposing_tractability"
-]
-
-def create_unmet_score_vectors(vectors: Dict[str, List[int]]):
-    unmet_scores_normalized = {}
-    for indication, scores in vectors.items():
-        for i in scores:
-            unmet_scores_normalized[unmet_needs[0]] = scores[i]
-    return unmet_scores_normalized
-
 def normalize_unmet_needs(unmet_vectors):
-    normalized_scores = {}
-    for __, scores in unmet_vectors:
-        res = []
-        for score in scores:
-            
-            min_score = min(scores)
-            max_score = max(scores)
-
-            res.append((score - min_score) / (max_score - min_score))
-            
-        for i in INDICATIONS:
-            normalized_scores[INDICATIONS[i]].append(res[i])
-
-    return normalized_scores
-
-print(normalize_unmet_needs(create_unmet_score_vectors(create_indications_as_vector())))
+    pass
--- src/score.py
+++ src/score.py
@@ -1,33 +1,38 @@
 import os
 import csv
-# Represent each 11 indications represented as a vector R7 of unmet needs.
-
-INDICATIONS = [
-    "GBM",
-    "AZ",
-    "IPF",
-    "SCD",
-    "GI-ARS",
-    "MPS-VI",
-    "BC",
-    "CAN",
-    "HX",
-    "BOF",
-    "BCC"
-]
 
 DATA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "data", "raw_scores.csv")
 
-def create_indications_as_vector():
+# Represent each 11 indications represented as a vector R7 of unmet needs.
+def create_indications_as_vector(file_path=DATA_PATH):
     indication_vectors = {}
-    with open(DATA_PATH, "r") as c:
+    with open(file_path, "r") as c:
         reader = csv.reader(c)
         header = next(reader)
+        print(header)
         
         for indication in reader: 
-            indication_vectors[indication[0]] = [w for w in indication[1:]]
+            indication_vectors[indication[0]] = [int(w) for w in indication[1:]]
             print(f"Created {indication[0]} as a vector ")
 
     return indication_vectors
 
+def create_unmet_score_vectors(file_path=DATA_PATH):
+    unmet_needs_vectors = {}
+    with open(file_path, "r") as c:
+        reader = csv.reader(c)
+        header = next(reader)
+        unmet_lst = [header[i] for i in range(len(header))]
+        for unmet_need in reader:
+            for i in range(1, len(header)):
+                unmet_needs_vectors.setdefault(unmet_lst[i], []).append(int(unmet_need[i]))
+                print(f"Mapped: {unmet_lst[i]} -> {unmet_need[i]}")
 
+    return unmet_needs_vectors
+        
+
+
+
+    
+print(create_indications_as_vector())
+print(create_unmet_score_vectors())
